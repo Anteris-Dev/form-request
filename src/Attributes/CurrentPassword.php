becdcbbc98eeaf3c64033b7f8628b67d66774500
Aidan Casey <aidan@caseyhouse.net>
2021-08-13 11:28:46 -0400

    Adds the ability to specify a guard with `CurrentPassword`.

--- src/Attributes/CurrentPassword.php
+++ src/Attributes/CurrentPassword.php
@@ -7,8 +7,17 @@ use Attribute;
 #[Attribute(Attribute::TARGET_PROPERTY)]
 class CurrentPassword implements Rule
 {
+    public function __construct(private ?string $guard = null)
+    {
+        //
+    }
+
     public function getRules(): array
     {
+        if (! is_null($this->guard)) {
+            return ['current_password:' . $this->guard];
+        }
+
         return ['current_password'];
     }
 }
--- tests/Attributes/CurrentPasswordTest.php
+++ tests/Attributes/CurrentPasswordTest.php
@@ -3,14 +3,31 @@
 namespace Anteris\Tests\FormRequest\Attributes;
 
 use Anteris\FormRequest\Attributes\CurrentPassword;
+use Anteris\Tests\FormRequest\Support\TestsValidationAttributes;
 use PHPUnit\Framework\TestCase;
 
 class CurrentPasswordTest extends TestCase
 {
-    public function test_it_returns_correct_rules()
+    use TestsValidationAttributes;
+
+    public function test_it_is_a_validation_attribute()
+    {
+        $this->assertValidationAttribute(CurrentPassword::class);
+    }
+
+    public function test_it_returns_correct_rules_without_guard()
     {
         $password = new CurrentPassword();
 
-        $this->assertSame(['current_password'], $password->getRules());
+        $this->assertValidationRules(['current_password'], $password);
+    }
+
+    public function test_it_returns_correct_rules_with_guard()
+    {
+        $password = new CurrentPassword('admin');
+        $password2 = new CurrentPassword('user');
+
+        $this->assertValidationRules(['current_password:admin'], $password);
+        $this->assertValidationRules(['current_password:user'], $password2);
     }
 }
